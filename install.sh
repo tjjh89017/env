e40c2edf0e049be5e143ae0bbe0119e759cad74d
Date Huang <tjjh89017@hotmail.com>
2014-08-18 19:05:05 +0800

    fix

--- install.sh
+++ install.sh
@@ -3,35 +3,35 @@
 #install powerline-shell for fish
 function install_powerline(){
 	echo Installing powerline-shell to "$HOME"/.powerline-shell/
-	exec git clone https://github.com/milkbikis/powerline-shell "$HOME"/.powerline-shell
-	exec "$HOME"/.powerline-shell/install.py
+	git clone https://github.com/milkbikis/powerline-shell "$HOME"/.powerline-shell
+	"$HOME"/.powerline-shell/install.py
 
 	echo Config fish config.fish
-	exec mv ./config.fish "$HOME"/.config/fish/
+	mv ./config.fish "$HOME"/.config/fish/
 }
 
 # install vimrc
 function install_vimrc(){
 	echo Installing vimrc to "$HOME"/.vimrc
-	exec mv ./vimrc "$HOME"/.vimrc
+	mv ./vimrc "$HOME"/.vimrc
 }
 
 # install Vundle.vim
 function install_Vundle.vim(){
 	echo Installing Vundle.vim to "$HOME"/.vim/bundle/Vundel.vim
-	exec 'git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim'
-	exec 'vim +PluginInstall +qall'
+	git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+	vim +PluginInstall +qall
 }
 
 # install Homebrew and some software indeed
 function install_Homebrew(){
-	echo Installing HomeBrew
-	exec 'ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"'
+	echo 'Installing HomeBrew'
+	ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 }
 
 # install some sofeware indeed
 # Todo: use list in file to do that better
 function install_brew_software(){
-	echo Installing software
-	exec 'brew install wget git fish'
+	echo 'Installing software'
+	brew install wget git fish
 }
