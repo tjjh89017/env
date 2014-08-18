537d8e4f337f59d45048124dd173a152e651d561
Date Huang <tjjh89017@hotmail.com>
2014-08-18 16:44:36 +0800

    function for install powerline-shell

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#install powerline-shell for fish
+function install_powerline(){
+	echo Installing powerline-shell to "$HOME"/.powerline-shell/
+}
+
 # install vimrc
 function install_vimrc(){
 	echo Installing vimrc to "$HOME"/.vimrc
