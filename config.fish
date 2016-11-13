c6338ef3bde1a9e12906e616579924fda08f1b7b
Date Huang <tjjh89017@hotmail.com>
2016-11-13 22:15:55 +0800

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,3 +1,8 @@
 function fish_prompt
 	~/.powerline-shell/powerline-shell.py $status --shell bare ^/dev/null
 end
+
+function __fish_cancel_commandline
+	commandline ""
+	commandline -f repaint
+end
